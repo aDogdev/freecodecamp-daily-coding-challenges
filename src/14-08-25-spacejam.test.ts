10f966ced9a81c80d9c71e4741dcb50c9671048e
adog <adrianmg3008@gmail.com>
2025-08-20 15:25:31 -0400

    update spacejam challenge with cicle solution

--- src/14-08-25-spacejam.test.ts
+++ src/14-08-25-spacejam.test.ts
@@ -4,6 +4,16 @@ function spaceJam(s: string): string {
   return s.split(" ").join("").split("").join("  ").toUpperCase();
 }
 
+function spaceJamCicle(s: string): string {
+  let str: string = "";
+  for (let char of s) {
+    if (char !== " ") {
+      str += char.toUpperCase() + "  ";
+    }
+  }
+  return str.trim();
+}
+
 describe("spaceJam", () => {
   it("should return the input string with two spaces between characters and an uppercase transformation", () => {
     expect(spaceJam("freeCodeCamp")).toBe("F  R  E  E  C  O  D  E  C  A  M  P");
@@ -15,5 +25,23 @@ describe("spaceJam", () => {
     );
     expect(spaceJam("C@t$ & D0g$")).toBe("C  @  T  $  &  D  0  G  $");
     expect(spaceJam("allyourbase")).toBe("A  L  L  Y  O  U  R  B  A  S  E");
+  });
+});
+
+describe("spaceJamCicle", () => {
+  it("should return the input string with two spaces between characters and an uppercase transformation", () => {
+    expect(spaceJamCicle("freeCodeCamp")).toBe(
+      "F  R  E  E  C  O  D  E  C  A  M  P"
+    );
+    expect(spaceJamCicle("   free   Code   Camp   ")).toBe(
+      "F  R  E  E  C  O  D  E  C  A  M  P"
+    );
+    expect(spaceJamCicle("Hello World?!")).toBe(
+      "H  E  L  L  O  W  O  R  L  D  ?  !"
+    );
+    expect(spaceJamCicle("C@t$ & D0g$")).toBe("C  @  T  $  &  D  0  G  $");
+    expect(spaceJamCicle("allyourbase")).toBe(
+      "A  L  L  Y  O  U  R  B  A  S  E"
+    );
   });
 });
